26a2ae3583cb3499c9475ef28913286e04ce01f8
Moshe Bildner <moshe.bildner@gmail.com>
2015-08-08 22:11:06 -0400

    basic string movement functionality working

---
+++ parser.js
@@ -0,0 +1,46 @@
+'use strict';
+
+function Parser (src){
+  var anchor = 0;
+  var scout = 0;
+  var self = this;
+
+  this.FINISHED = false;
+  this.consumeTo = consumeTo;
+
+  function string (){
+    var str = src.slice(anchor, scout);
+    return str;
+  }
+
+  function currentChar (){
+    return src.charAt(scout);
+  }
+
+  function finish (){
+    anchor = scout;
+  }
+
+  function advance (){
+    if (scout >= src.length){
+      self.FINISHED = true;
+    }
+    scout += 1;
+  }
+
+  function advanceTo (char){
+    while (currentChar() !== char){
+      advance();
+      if (self.FINISHED){ break; }
+    }
+  }
+
+  function consumeTo (char){
+    advanceTo(char);
+    var str = string();
+    finish();
+    return str;
+  }
+}
+
+module.exports = Parser;
---
+++ spec/parser_spec.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var fs = require('fs');
+var Parser = require('../parser.js');
+
+var demo = fs.readFileSync('./demo.ms', { encoding: 'UTF-8' });
+
+beforeEach(function(){
+});
+
+describe('#consumeTo', function(){
+  var parser;
+
+  beforeEach(function(){
+    parser = new Parser('first this# then this.');
+  });
+
+  it('returns a string up to the character', function(){
+    var first = parser.consumeTo('#');
+    var second = parser.consumeTo('.');
+
+    expect(first).toBe('first this');
+    expect(second).toBe('# then this');
+  });
+});
+
+describe('FINISHED', function(){
+
+});

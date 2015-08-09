fd942ab58761ff09cd5cd12040cb71fbf54f300d
Moshe Bildner <moshe.bildner@gmail.com>
2015-08-09 17:42:04 -0400

    reformat parser spec

--- spec/parser_spec.js
+++ spec/parser_spec.js
@@ -2,51 +2,54 @@
 
 var Parser = require('../parser.js');
 
-describe('#consumeTo', function(){
-  var parser;
+describe('Parser', function(){
+  describe('#consumeTo', function(){
+    var parser;
 
-  beforeEach(function(){
-    parser = new Parser('first this# then this.');
-  });
-
-  context('FINISHED', function(){
     beforeEach(function(){
-     parser.consumeTo('then this.');
+      parser = new Parser('first this# then this.');
     });
 
-    it('returns null', function(){
-      expect(parser.consumeTo('a')).toBeNull();
+    context('FINISHED', function(){
+      beforeEach(function(){
+        parser.consumeTo('then this.');
+      });
+
+      it('returns null', function(){
+        expect(parser.consumeTo('a')).toBeNull();
+      });
+    });
+
+    context('single character', function(){
+      it('returns a string through the character', function(){
+        var first = parser.consumeTo('#');
+        var second = parser.consumeTo('.');
+
+        expect(first).toBe('first this#');
+        expect(second).toBe(' then this.');
+      });
+    });
+
+    context('longer string', function(){
+      it('returns a string through the test string', function(){
+        var first = parser.consumeTo('this');
+        var second = parser.consumeTo('this.');
+
+        expect(first).toBe('first this');
+        expect(second).toBe('# then this.');
+      });
+    });
+
+    context('regex', function(){
+      it('returns a string through the regex', function(){
+        var first = parser.consumeTo(/this[\.#]{1}/);
+        var second = parser.consumeTo(/this[\.#]{1}/);
+
+        expect(first).toBe('first this#');
+        expect(second).toBe(' then this.');
+      });
     });
   });
 
-  context('single character', function(){
-    it('returns a string through the character', function(){
-      var first = parser.consumeTo('#');
-      var second = parser.consumeTo('.');
 
-      expect(first).toBe('first this#');
-      expect(second).toBe(' then this.');
-    });
-  });
-
-  context('longer string', function(){
-    it('returns a string through the test string', function(){
-      var first = parser.consumeTo('this');
-      var second = parser.consumeTo('this.');
-
-      expect(first).toBe('first this');
-      expect(second).toBe('# then this.');
-    });
-  });
-
-  context('regex', function(){
-    it('returns a string through the regex', function(){
-      var first = parser.consumeTo(/this[\.#]{1}/);
-      var second = parser.consumeTo(/this[\.#]{1}/);
-
-      expect(first).toBe('first this#');
-      expect(second).toBe(' then this.');
-    });
-  });
 });
-

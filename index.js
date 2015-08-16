fe870f37cfbc7b2a8fc99868b7c5be2628fe356a
Moshe Bildner <moshe.bildner@gmail.com>
2015-08-16 13:49:49 -0400

    composite tokenizer marks keywords

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ var CompositeTokenizer = require('./lib/composite_tokenizer.js');
 var fs = require('fs');
 var ms = fs.readFileSync('./spec/fixtures/demo.ms', { encoding: 'UTF-8' });
 
-var tokenizer = new CompositeTokenizer(ms);
+function ExpressionFactory (src){
+  var tokenizer = new CompositeTokenizer(ms);
 
-var token;
-while ((token = tokenizer.consumeNext())){
-  console.log(token.toString());
+  this.consumeNext = function(){
+    var token = tokenizer.consumeNext();
+    if (!token) { return false; }
+
+    return token;
+  };
+
+}
+
+var expressions = new ExpressionFactory(ms);
+
+var expression;
+while((expression = expressions.consumeNext())){
+  console.log(expression.toString());
 }
 

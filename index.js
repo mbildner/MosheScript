f030c5759ee371e9d95c9ffe8fd17b8299c9f180
Moshe Bildner <moshe.bildner@gmail.com>
2015-08-18 00:16:02 -0400

    testing parser strategies

--- index.js
+++ index.js
@@ -2,23 +2,42 @@ var CompositeTokenizer = require('./lib/composite_tokenizer.js');
 
 var fs = require('fs');
 var ms = fs.readFileSync('./spec/fixtures/demo.ms', { encoding: 'UTF-8' });
+var types = require('./lib/types.js');
 
 function ExpressionFactory (src){
   var tokenizer = new CompositeTokenizer(ms);
 
-  this.consumeNext = function(){
-    var token = tokenizer.consumeNext();
-    if (!token) { return false; }
+  var current;
+  var next = tokenizer.consumeNext();
 
-    return token;
+  // fast forward whitespace
+  while (next.type === types.WHITESPACE || next.type === types.NEW_LINE){
+    next = tokenizer.consumeNext();
+  }
+
+  this.advance = function(){
+    // original next is never whitespace
+    current = next;
+
+    do {
+      next = tokenizer.consumeNext();
+    } while (next.type === types.WHITESPACE || next.type === types.NEW_LINE);
+
+    return current;
   };
 
+  this.current = function() { return current; };
+  this.peek = function() { return next; };
 }
 
 var expressions = new ExpressionFactory(ms);
 
 var expression;
-while((expression = expressions.consumeNext())){
-  console.log(expression.toString());
+while((expression = expressions.advance())){
+
+  console.log(expressions.current());
+  console.log(expressions.peek());
+
+
 }
 
